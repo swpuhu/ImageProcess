7df16cd3465bcdd731dffa88528c9419fa24d2af
huhu <swpujita@126.com>
2019-03-12 13:10:18 +0800

    correct gama worker

--- src/js/worker/gama.js
+++ src/js/worker/gama.js
@@ -2,8 +2,10 @@ onmessage = function (e) {
     let imageData = e.data.imageData;
     let co = e.data.params.co;
     let exp = e.data.params.exp;
-    for (let i = 0; i < imageData.data.length; i++) {
+    for (let i = 0; i < imageData.data.length; i += 4) {
         imageData.data[i] = Math.round(co * Math.pow(imageData.data[i] / 255, exp) * 255);
+        imageData.data[i + 1] = Math.round(co * Math.pow(imageData.data[i + 1] / 255, exp) * 255);
+        imageData.data[i + 2] = Math.round(co * Math.pow(imageData.data[i + 2] / 255, exp) * 255);
     }
     postMessage(imageData);
 }
